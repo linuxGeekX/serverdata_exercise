b37b270480acb9fba02fcff3d74f0dad6e5a1f60
Oscar Strookman <oscar.strookman@headforwards.com>
2016-12-20 14:58:57 +0000

    Finished up the task.

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 (function($) {
-	
+	function JSONLoaded(data, err, jqXHR) {
+		function BuildRow(index, server) {
+			if(index == 0) {
+				console.log(server);
+			}
+			$('table#serverTable').append('<tr id="' + index + '"></tr>');
+			var htmlStr = '<td>' + server.name + '</td><td>'
+			if(server.cmp_state == 'running')
+				htmlStr += '<div class="statButton"></div>'
+			htmlStr += '</td><td>' + server.account.name + '</td><td>' + server.location.datacenter.name + '</td><td>';
+			if(server.cpu_cores === null)
+				htmlStr += 'N/A';
+			else
+				htmlStr += server.cpu_cores;
+			htmlStr += '</td><td>N/A</td><td>';
+			if(server.ram_mb === null)
+				htmlStr += 'N/A';
+			else
+				htmlStr += server.ram_mb;
+			htmlStr += '</td><td>'
+			if(server.volumes_mb === null)
+				htmlStr += 'N/A';
+			else
+				htmlStr += server.volumes_mb;
+			htmlStr += '</td>';
+			$('tr#' + index).append(htmlStr);
+		}
+
+		$.each(data, BuildRow);
+	}
+
+	$.getJSON("table.json", JSONLoaded);
 })(jQuery);
